b456a7144af44b4f0c168f2db033734192a69236
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-12-02 19:40:19 -0500

    passed tests

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,35 @@
 package rocks.zipcode.io;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    private Item[] itemArr;
     public ItemSorter(Item[] items) {
+        this.itemArr = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        // comment out code with explanation
+        for (int i = 0; i < itemArr.length -1; i++) {
+            boolean b = false;
+            for (int j = 0; j < itemArr.length -1; j++) {
+                if (comparator.compare(itemArr[j], itemArr[j + 1]) > 0) {
+                    Item it = itemArr[j];
+                    itemArr[j] = itemArr[j+1];
+                    itemArr[j+1] = it;
+                    b = true;
+                }
+            }
+            if (!b) {
+                break;
+            }
+        }
+        return itemArr;
+//        return null;
     }
 }
