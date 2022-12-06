1d4c39373b9d50814e48ff70aa759083123734d0
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-12-06 17:52:41 -0500

    added comment

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -9,7 +9,7 @@ import java.util.Comparator;
  */
 public class NameComparator implements Comparator<Item> {
     @Override
-    public int compare(Item o1, Item o2) {
+    public int compare(Item o1, Item o2) { // sorted alphabetically
         return o1.getName().compareTo(o2.getName());
     }
 }
